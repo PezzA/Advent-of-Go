190f0aed6d1985d7785f45a8db7943e3d23301a6
jperrin <jperrin@travelrepublic.co.uk>
2017-11-19 17:42:53 +0000

    just finishing off day 5

--- Day201505/main.go
+++ Day201505/main.go
@@ -1,16 +1,112 @@
 package Day201505
 
+import (
+	"bufio"
+	"strconv"
+	"strings"
+)
+
 // Entry holds wraps the data and runner interfaces for this puzzle
 var Entry testDay
 
 type testDay bool
 
+var vowels = "aeiou"
+var invalidMatches map[string]bool
+
+func init() {
+	invalidMatches = make(map[string]bool, 0)
+	invalidMatches["ab"] = true
+	invalidMatches["cd"] = true
+	invalidMatches["pq"] = true
+	invalidMatches["xy"] = true
+}
+
+func sufficentVowels(input string) bool {
+	count := 0
+
+	for _, char := range input {
+		if strings.ContainsRune(vowels, char) {
+			count++
+		}
+	}
+
+	return count >= 3
+}
+
+func hasRepeat(input string) bool {
+	for i := 0; i < len(input)-1; i++ {
+		if input[i] == input[i+1] {
+			return true
+		}
+	}
+	return false
+}
+
+func hasBumpy(input string) bool {
+	for i := 0; i < len(input)-2; i++ {
+		if input[i] == input[i+2] {
+			return true
+		}
+	}
+	return false
+}
+
+func noInvalidMatches(input string) bool {
+	for i := 0; i < len(input)-1; i++ {
+		if _, ok := invalidMatches[input[i:i+2]]; ok {
+			return false
+		}
+	}
+	return true
+}
+
+func hasRepeatNoOverlap(input string) bool {
+	bits := make(map[string]int, 0)
+
+	for i := 0; i < len(input)-1; i++ {
+		if _, ok := bits[input[i:i+2]]; ok {
+			bits[input[i:i+2]]++
+		} else {
+			bits[input[i:i+2]] = 1
+		}
+
+		if input[i] == input[i+1] {
+			i++
+		}
+	}
+
+	for _, val := range bits {
+		if val > 1 {
+			return true
+		}
+	}
+
+	return false
+}
+
 func (td testDay) PartOne(inputData string) (string, error) {
-	return " -- Not Yet Implemented --", nil
+	total := 0
+	scanner := bufio.NewScanner(strings.NewReader(inputData))
+	for scanner.Scan() {
+		testText := scanner.Text()
+		if sufficentVowels(testText) && noInvalidMatches(testText) && hasRepeat(testText) {
+			total++
+		}
+	}
+	return strconv.Itoa(total), nil
 }
 
 func (td testDay) PartTwo(inputData string) (string, error) {
-	return " -- Not Yet Implemented --", nil
+	total := 0
+	scanner := bufio.NewScanner(strings.NewReader(inputData))
+	for scanner.Scan() {
+		testText := scanner.Text()
+		if hasBumpy(testText) && hasRepeatNoOverlap(testText) {
+			total++
+		}
+	}
+	return strconv.Itoa(total), nil
 }
 
 func (td testDay) Day() int {
@@ -29,5 +125,13 @@ func (td testDay) GetTestData() ([]string, []string) {
 			"haegwjzuvuyypxyu",
 			"dvszwmarrgswjxmb",
 		},
-		[]string{}
+		[]string{
+			"qjhvhtzxzqqjkmpb",
+			"xxyxx",
+			"uurcxstgmygtbstg",
+			"ieodomkazucvgmuy",
+			"aaa",
+			"aaaa",
+			"xilodxfuxphuiiii",
+		}
 }
