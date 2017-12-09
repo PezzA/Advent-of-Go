8aef35483566e86a264c9263a743daac1a82c4d7
jperrin <jperrin@travelrepublic.co.uk>
2017-12-09 06:56:05 +0000

    Day 9

--- 2017/Day201709/main.go
+++ 2017/Day201709/main.go
@@ -1,5 +1,11 @@
 package Day201709
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // Entry holds wraps the data and runner interfaces for this puzzle
 var Entry testDay
 
@@ -10,12 +16,70 @@ type node struct {
 	nodes []node
 }
 
+func isSpecialChar(char rune) bool {
+	return char == '{' || char == '}' || char == '!' || char == '<' || char == '>'
+}
+
+func streamReader(input string, score int, cumlativeScore int, garbageCount int) (node, int, int, int) {
+	node := node{score, nil}
+
+	for i := 0; i < len(input); i++ {
+		currentRune := rune(input[i])
+		if isSpecialChar(currentRune) {
+			switch rune(currentRune) {
+			case '{':
+				childNode, subIndex, newCumlatiuve, newgarbageCount := streamReader(input[i+1:], score+1, cumlativeScore, garbageCount)
+				cumlativeScore = newCumlatiuve
+				garbageCount = newgarbageCount
+				node.nodes = append(node.nodes, childNode)
+				i += subIndex
+			case '}':
+				return node, i + 1, cumlativeScore + node.score, garbageCount
+			case '!':
+				i++
+			case '<':
+				terminate := false
+				for !terminate {
+					i++
+					subRune := rune(input[i])
+					switch subRune {
+					case '!':
+						i++
+					case '>':
+						terminate = true
+					default:
+						garbageCount++
+					}
+				}
+			}
+		}
+	}
+
+	return node, 0, cumlativeScore, garbageCount
+}
+
+func printTree(n node, depth int) {
+	fmt.Println(strings.Repeat(" ", depth*4), "G -", n.score)
+	for _, childNode := range n.nodes {
+		printTree(childNode, depth+1)
+	}
+}
+
+func scoreTree(n node, score int) int {
+	for _, childNode := range n.nodes {
+		score += scoreTree(childNode, score)
+	}
+	return score + n.score
+}
+
 func (td testDay) PartOne(inputData string) (string, error) {
-	return " -- Not Yet Implemented --", nil
+	_, _, cumlative, _ := streamReader(inputData, 0, 0, 0)
+	return strconv.Itoa(cumlative), nil
 }
 
 func (td testDay) PartTwo(inputData string) (string, error) {
-	return " -- Not Yet Implemented --", nil
+	_, _, _, garbageCount := streamReader(inputData, 0, 0, 0)
+	return strconv.Itoa(garbageCount), nil
 }
 
 func (td testDay) Day() int {
--- 2017/Day201709/main_test.go
+++ 2017/Day201709/main_test.go
@@ -9,6 +9,33 @@ import (
 func Test_PartOne(t *testing.T) {
 	RegisterTestingT(t)
 
+	var tree node
+	_, _, cumulative, _ := streamReader("{}", 0, 0, 0)
+	Expect(cumulative).Should(Equal(1))
+
+	_, _, cumulative, _ = streamReader("{{{}}}", 0, 0, 0)
+	Expect(cumulative).Should(Equal(6))
+
+	_, _, cumulative, _ = streamReader("{{},{}}", 0, 0, 0)
+	Expect(cumulative).Should(Equal(5))
+
+	_, _, cumulative, _ = streamReader("{{{},{},{{}}}}", 0, 0, 0)
+	Expect(cumulative).Should(Equal(16))
+
+	_, _, cumulative, _ = streamReader("{<a>,<a>,<a>,<a>}", 0, 0, 0)
+	Expect(cumulative).Should(Equal(1))
+
+	_, _, cumulative, _ = streamReader("{{<ab>},{<ab>},{<ab>},{<ab>}}", 0, 0, 0)
+	Expect(cumulative).Should(Equal(9))
+
+	_, _, cumulative, _ = streamReader("{{<!!>},{<!!>},{<!!>},{<!!>}}", 0, 0, 0)
+	Expect(cumulative).Should(Equal(9))
+
+	tree, _, cumulative, _ = streamReader("{{<a!>},{<a!>},{<a!>},{<ab>}}", 0, 0, 0)
+
+	printTree(tree, 0)
+	Expect(cumulative).Should(Equal(3))
+
 }
 
 func Test_PartTwo(t *testing.T) {
