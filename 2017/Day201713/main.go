60c7a6a315f5f55f3e459dde8b892585c74ef409
jperrin <jperrin@travelrepublic.co.uk>
2017-12-13 11:08:52 +0000

    day 13

--- 2017/Day201713/main.go
+++ 2017/Day201713/main.go
@@ -1,16 +1,96 @@
 package Day201713
 
+import (
+	"bufio"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 // Entry holds wraps the data and runner interfaces for this puzzle
 var Entry testDay
 
+type layers map[int]int
+
+var layerRegex = regexp.MustCompile(`([0-9]+): ([0-9]+)`)
+
 type testDay bool
 
+func getLayers(input string) (layers, int) {
+
+	maxdepth := 0
+	layerMap := make(layers, 0)
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	for scanner.Scan() {
+		matches := layerRegex.FindStringSubmatch(scanner.Text())
+
+		depth, _ := strconv.Atoi(matches[1])
+
+		if depth > maxdepth {
+			maxdepth = depth
+		}
+		layerRange, _ := strconv.Atoi(matches[2])
+
+		layerMap[depth] = layerRange
+	}
+
+	return layerMap, maxdepth
+}
+
+func getPositionForTime(time int, length int) int {
+	if length == 1 {
+		return 1
+	}
+	maxsteps := (2 * length) - 2
+	step := time % maxsteps
+	if step >= length {
+		return maxsteps - step
+	}
+	return step
+}
+
 func (td testDay) PartOne(inputData string) (string, error) {
-	return " -- Not Yet Implemented --", nil
+	layermap, maxDepth := getLayers(inputData)
+	picoSeconds := 0
+	severity := 0
+	for i := 0; i <= maxDepth; i++ {
+
+		if val, ok := layermap[i]; ok {
+			if getPositionForTime(picoSeconds, val) == 0 {
+				severity += (i * val)
+			}
+		}
+
+		picoSeconds++
+	}
+	return strconv.Itoa(severity), nil
 }
 
 func (td testDay) PartTwo(inputData string) (string, error) {
-	return " -- Not Yet Implemented --", nil
+	layermap, maxDepth := getLayers(inputData)
+	seedPicoseconds := 0
+	for {
+		picoSeconds := seedPicoseconds
+		valid := true
+		for i := 0; i <= maxDepth; i++ {
+
+			if val, ok := layermap[i]; ok {
+				if getPositionForTime(picoSeconds, val) == 0 {
+					valid = false
+				}
+			}
+
+			if !valid {
+				break
+			}
+			picoSeconds++
+		}
+		if valid {
+			break
+		}
+		seedPicoseconds++
+	}
+	return strconv.Itoa(seedPicoseconds), nil
 }
 
 func (td testDay) Day() int {
@@ -18,13 +98,9 @@ func (td testDay) Day() int {
 }
 
 func (td testDay) Year() int {
-	return 0
+	return 2017
 }
 
 func (td testDay) Title() string {
 	return "Getting the boilerplate in place"
-}
-
-func (td testDay) GetData() string {
-	return "Actual Data"
 }
--- 2017/Day201713/main_test.go
+++ 2017/Day201713/main_test.go
@@ -8,12 +8,19 @@ import (
 
 func Test_PartOne(t *testing.T) {
 	RegisterTestingT(t)
-
+	Expect(getPositionForTime(0, 4)).Should(Equal(0))
+	Expect(getPositionForTime(1, 4)).Should(Equal(1))
+	Expect(getPositionForTime(2, 4)).Should(Equal(2))
+	Expect(getPositionForTime(3, 4)).Should(Equal(3))
+	Expect(getPositionForTime(4, 4)).Should(Equal(2))
+	Expect(getPositionForTime(5, 4)).Should(Equal(1))
+	Expect(getPositionForTime(6, 4)).Should(Equal(0))
+	Expect(getPositionForTime(7, 4)).Should(Equal(1))
+	Expect(getPositionForTime(8, 4)).Should(Equal(2))
 }
 
 func Test_PartTwo(t *testing.T) {
 	RegisterTestingT(t)
-
 }
 
 func Benchmark_PartOne(b *testing.B) {
