88991a034760452b8404ca4b30bc21b4bec82083
jperrin <jperrin@travelrepublic.co.uk>
2017-12-18 09:37:32 +0000

    Day 18 - csp for the win!

--- 2017/Day201718/main.go
+++ 2017/Day201718/main.go
@@ -1,21 +1,230 @@
 package Day201718
 
+import (
+	"bufio"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
 type dayEntry bool
 
 var Entry dayEntry
 
 func (td dayEntry) Describe() (int, int, string) {
-	return 2017, 18, "Getting the boilerplate in place"
+	return 2017, 18, "Duet"
+}
+
+type instruction struct {
+	command  string
+	operand1 string
+	operand2 string
+}
+
+type registers map[string]int
+
+type chip struct {
+	registers        registers
+	lastSound        int
+	recoveredSound   int
+	index            int
+	sendChannel      chan int
+	receiveChannel   chan int
+	messagesSent     int
+	messagesReceived int
+	deadlocked       bool
+	instructions     int
+}
+
+var instructionRegex = regexp.MustCompile(`(set|mul|jgz|add|mod|snd|rcv) (\S+)( )*(\S*)`)
+
+func loadInstructions(input string) []instruction {
+
+	ins := make([]instruction, 0)
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		matches := instructionRegex.FindStringSubmatch(line)
+
+		ins = append(ins, instruction{matches[1], matches[2], matches[4]})
+	}
+
+	return ins
+}
+
+func (c chip) getRegister(reg string) int {
+	if val, ok := c.registers[reg]; ok {
+		return val
+	}
+
+	c.registers[reg] = 0
+	return 0
+}
+
+func (c chip) runInstruction(i instruction) chip {
+
+	switch i.command {
+	case "set":
+		targetVal, err := strconv.Atoi(i.operand2)
+
+		if err != nil {
+			c.registers[i.operand1] = c.getRegister(i.operand2)
+		} else {
+			c.registers[i.operand1] = targetVal
+		}
+		c.index++
+	case "snd":
+		if c.sendChannel != nil {
+			targetVal, err := strconv.Atoi(i.operand1)
+
+			if err != nil {
+				targetVal = c.getRegister(i.operand1)
+			}
+
+			c.sendChannel <- targetVal
+			c.messagesSent++
+			c.index++
+			break
+		}
+
+		c.lastSound = c.getRegister(i.operand1)
+		c.index++
+	case "add":
+		targetVal, err := strconv.Atoi(i.operand2)
+
+		if err != nil {
+			c.registers[i.operand1] += c.getRegister(i.operand2)
+		} else {
+			c.registers[i.operand1] += targetVal
+		}
+		c.index++
+	case "mul":
+		targetVal, err := strconv.Atoi(i.operand2)
+
+		if err != nil {
+			c.registers[i.operand1] *= c.getRegister(i.operand2)
+		} else {
+			c.registers[i.operand1] *= targetVal
+		}
+		c.index++
+	case "mod":
+		targetVal, err := strconv.Atoi(i.operand2)
+
+		if err != nil {
+			c.registers[i.operand1] = c.registers[i.operand1] % c.getRegister(i.operand2)
+		} else {
+			c.registers[i.operand1] = c.registers[i.operand1] % targetVal
+		}
+		c.index++
+	case "rcv":
+		if c.receiveChannel != nil {
+
+			select {
+			case intVal := <-c.receiveChannel:
+				c.registers[i.operand1] = intVal
+				c.index++
+				c.messagesReceived++
+			case <-time.After(time.Millisecond * 10):
+				c.deadlocked = true
+			}
+
+			break
+		}
+
+		if c.getRegister(i.operand1) == 0 {
+			c.index++
+			break
+		}
+
+		c.recoveredSound = c.lastSound
+		c.index++
+	case "jgz":
+		val, err := strconv.Atoi(i.operand1)
+
+		if err != nil {
+			val = c.getRegister(i.operand1)
+		}
+
+		if val > 0 {
+			shift, err := strconv.Atoi(i.operand2)
+
+			if err != nil {
+				c.index += c.getRegister(i.operand2)
+			}
+
+			c.index += shift
+			break
+		}
+		c.index++
+	}
+
+	c.instructions++
+	return c
+}
+
+func runProgram(ins []instruction, c chip, progressChannel chan int, updateChan chan []string) int {
+	for {
+		c = c.runInstruction(ins[c.index])
+
+		if c.recoveredSound > 0 || c.index < 0 || c.index > len(ins) || c.deadlocked {
+			break
+		}
+
+	}
+
+	if progressChannel != nil {
+		progressChannel <- c.messagesSent
+	}
+	return c.recoveredSound
 }
 
 func (td dayEntry) PartOne(inputData string, updateChan chan []string) (string, error) {
-	return " -- Not Yet Implemented --", nil
+	c := chip{
+		registers:      make(registers, 0),
+		lastSound:      0,
+		recoveredSound: 0,
+		index:          0,
+	}
+
+	val := runProgram(loadInstructions(inputData), c, nil, nil)
+
+	return strconv.Itoa(val), nil
 }
 
 func (td dayEntry) PartTwo(inputData string, updateChan chan []string) (string, error) {
-	return " -- Not Yet Implemented --", nil
-}
 
-func (td dayEntry) PuzzleInput() string {
-	return "Actual Data"
+	atob := make(chan int, 10000)
+	btoa := make(chan int, 10000)
+
+	c := chip{
+		registers:      make(registers, 0),
+		lastSound:      0,
+		recoveredSound: 0,
+		index:          0,
+		sendChannel:    atob,
+		receiveChannel: btoa,
+	}
+	c.registers["p"] = 0
+	c2 := chip{
+		registers:      make(registers, 0),
+		lastSound:      0,
+		recoveredSound: 0,
+		index:          0,
+		sendChannel:    btoa,
+		receiveChannel: atob,
+	}
+	c2.registers["p"] = 1
+
+	progress1 := make(chan int, 0)
+	progress2 := make(chan int, 0)
+
+	go runProgram(loadInstructions(inputData), c, progress1, nil)
+	go runProgram(loadInstructions(inputData), c2, progress2, updateChan)
+
+	<-progress1
+	prog1messages := <-progress2
+
+	return strconv.Itoa(prog1messages), nil
 }
--- 2017/Day201718/main_test.go
+++ 2017/Day201718/main_test.go
@@ -9,23 +9,85 @@ import (
 func Test_PartOne(t *testing.T) {
 	RegisterTestingT(t)
 
+	ins := loadInstructions(`set a 1
+add a 2
+mul a a
+mod a 5
+snd a
+set a 0
+rcv a
+jgz a -1
+set a 1
+jgz a -2`)
+
+	c := chip{
+		registers:      make(registers, 0),
+		lastSound:      0,
+		recoveredSound: 0,
+		index:          0,
+	}
+
+	val := runProgram(ins, c, nil, nil)
+
+	Expect(val).Should(Equal(4))
 }
 
 func Test_PartTwo(t *testing.T) {
 	RegisterTestingT(t)
 
+	ins := loadInstructions(`snd 1
+		snd 2
+		snd p
+		rcv a
+		rcv b
+		rcv c
+		rcv d`)
+
+	atob := make(chan int, 10)
+	btoa := make(chan int, 10)
+
+	c := chip{
+		registers:      make(registers, 0),
+		lastSound:      0,
+		recoveredSound: 0,
+		index:          0,
+		sendChannel:    atob,
+		receiveChannel: btoa,
+	}
+
+	c.registers["p"] = 0
+	c2 := chip{
+		registers:      make(registers, 0),
+		lastSound:      0,
+		recoveredSound: 0,
+		index:          0,
+		sendChannel:    btoa,
+		receiveChannel: atob,
+	}
+	c2.registers["p"] = 1
+
+	progress1 := make(chan int, 0)
+	progress2 := make(chan int, 0)
+
+	go runProgram(ins, c, progress1, nil)
+	go runProgram(ins, c2, progress2, nil)
+
+	<-progress1
+	prog1messages := <-progress2
+
+	Expect(prog1messages).Should(Equal(3))
 }
 
 func Benchmark_PartOne(b *testing.B) {
 	data := Entry.PuzzleInput()
 	for n := 0; n < b.N; n++ {
-		Entry.PartOne(data)
+		Entry.PartOne(data, nil)
 	}
 }
 
 func Benchmark_PartTwo(b *testing.B) {
 	data := Entry.PuzzleInput()
 	for n := 0; n < b.N; n++ {
-		Entry.PartTwo(data)
+		Entry.PartTwo(data, nil)
 	}
 }
