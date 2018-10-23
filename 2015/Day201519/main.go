2c29279e60bfd2217a6f7f53ee8d351482562bf2
John Perrin <jperrin@travelrepublic.co.uk>
2018-10-23 12:25:03 +0100

    2015 19 pending

--- 2015/Day201519/main.go
+++ 2015/Day201519/main.go
@@ -3,14 +3,21 @@ package Day201519
 import (
 	"fmt"
 	"strings"
+
+	"github.com/pezza/advent-of-code/Common"
 )
 
 var Entry dayEntry
 
-type replacements map[string]string
+type replacement struct {
+	key         string
+	replacement string
+}
+
+type replacements []replacement
 
 func getData(input string) (replacements, string) {
-	replacementList, medicineMolecule := make(replacements), ""
+	replacementList, medicineMolecule := make(replacements, 0), ""
 
 	for _, line := range strings.Split(input, "\n") {
 		if line == "" {
@@ -19,7 +26,7 @@ func getData(input string) (replacements, string) {
 
 		if strings.Contains(line, "=>") {
 			bits := strings.Fields(line)
-			replacementList[bits[0]] = bits[2]
+			replacementList = append(replacementList, replacement{bits[0], bits[2]})
 			continue
 		}
 
@@ -35,7 +42,34 @@ func (td dayEntry) Describe() (int, int, string) {
 	return 2015, 19, "Medicine for Rudolph"
 }
 func (td dayEntry) PartOne(inputData string, updateChan chan []string) string {
-	return fmt.Sprintf(" -- Not Yet Implemented --")
+	replacementList, molecule := getData(inputData)
+	molecules := make([]string, 0)
+
+	for _, r := range replacementList {
+		foundIndex, currentPosition := 0, 0
+
+		for {
+			foundIndex = strings.Index(molecule[currentPosition:], r.key)
+			if foundIndex == -1 {
+				break
+			}
+			currentPosition += foundIndex + 1
+
+			newMolecule := molecule[:currentPosition-1]
+			newMolecule += r.replacement
+			newMolecule += molecule[currentPosition:]
+
+			if !common.Contains(molecules, newMolecule) {
+				molecules = append(molecules, newMolecule)
+			}
+		}
+	}
+
+	for _, val := range molecules {
+		fmt.Print(len(val), " ")
+	}
+
+	return fmt.Sprintf("Not 603. %v", len(molecules))
 }
 
 func (td dayEntry) PartTwo(inputData string, updateChan chan []string) string {
--- 2015/Day201519/main_test.go
+++ 2015/Day201519/main_test.go
@@ -2,18 +2,55 @@ package Day201519
 
 import (
 	"fmt"
+	"strings"
 	"testing"
+
+	"github.com/pezza/advent-of-code/Common"
 
 	. "github.com/onsi/gomega"
 )
 
+func getTestData() (replacements, string) {
+	replacementList := make(replacements, 0)
+	replacementList = append(replacementList, replacement{"H", "HO"})
+	replacementList = append(replacementList, replacement{"H", "OH"})
+	replacementList = append(replacementList, replacement{"O", "HH"})
+
+	return replacementList, "HOHOHO"
+}
+
 func Test_PartOne(t *testing.T) {
 	RegisterTestingT(t)
 
-	replacements, molecule := getData(Entry.PuzzleInput())
+	replacementList, molecule := getTestData()
 
 	fmt.Println("Medicine molecule", molecule)
-	fmt.Println("Replacement List", replacements)
+	fmt.Println("Replacement List", replacementList)
+
+	molecules := make([]string, 0)
+
+	for _, r := range replacementList {
+
+		foundIndex, currentPosition := 0, 0
+
+		for {
+			foundIndex = strings.Index(molecule[currentPosition:], r.key)
+			if foundIndex == -1 {
+				break
+			}
+			currentPosition += foundIndex + 1
+
+			newMolecule := molecule[:currentPosition-1]
+			newMolecule += r.replacement
+			newMolecule += molecule[currentPosition:]
+
+			if !common.Contains(molecules, newMolecule) {
+				molecules = append(molecules, newMolecule)
+			}
+		}
+	}
+
+	fmt.Println(len(molecules))
 }
 
 func Test_PartTwo(t *testing.T) {
