396e8fc688c6490dfe336f145eb5c5d0de978879
PezzA <john.d.perrin@gmail.com>
2018-12-01 05:28:20 +0000

    2018 day 1 complete

--- 2018/Day201801/main.go
+++ 2018/Day201801/main.go
@@ -1,6 +1,10 @@
 package Day201801
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 // Entry holds wraps the data and runner interfaces for this puzzle
 var Entry dayEntry
@@ -8,12 +12,47 @@ var Entry dayEntry
 type dayEntry bool
 
 func (td dayEntry) Describe() (int, int, string) {
-	return 2018, 1, "Getting the boilerplate in place"
+	return 2018, 1, "Chronal Calibration"
+}
+
+func getData(input string) []int {
+
+	vals := make([]int, 0)
+
+	for _, val := range strings.Split(input, "\n") {
+		testVal, _ := strconv.Atoi(val)
+
+		vals = append(vals, testVal)
+	}
+
+	return vals
+
 }
 func (td dayEntry) PartOne(inputData string, updateChan chan []string) string {
-	return fmt.Sprintf(" -- Not Yet Implemented --")
+	retVal := 0
+	for _, value := range getData(inputData) {
+		retVal += value
+	}
+	return fmt.Sprintf("%v", retVal)
 }
 
 func (td dayEntry) PartTwo(inputData string, updateChan chan []string) string {
-	return fmt.Sprintf(" -- Not Yet Implemented --")
+	freqs := make(map[int]bool)
+
+	retVal := 0
+	found := false
+
+	for !found {
+		for _, value := range getData(inputData) {
+			retVal += value
+
+			if _, ok := freqs[retVal]; ok {
+				found = true
+				break
+			}
+
+			freqs[retVal] = true
+		}
+	}
+	return fmt.Sprintf("%v", retVal)
 }
