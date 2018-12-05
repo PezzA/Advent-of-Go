ed15d060f4ab18d99c8e6b77ab2bb28dc60414a7
PezzA <john.d.perrin@gmail.com>
2018-12-05 06:16:13 +0000

    2018 day 05 done

--- 2018/Day201805/main.go
+++ 2018/Day201805/main.go
@@ -1,6 +1,9 @@
 package Day201805
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Entry holds wraps the data and runner interfaces for this puzzle
 var Entry dayEntry
@@ -8,12 +11,52 @@ var Entry dayEntry
 type dayEntry bool
 
 func (td dayEntry) Describe() (int, int, string) {
-	return 2018, 5, "Getting the boilerplate in place"
+	return 2018, 5, "Alchemical Reduction"
 }
+
+func reactPolymer(input string) (string, bool) {
+	for i := 0; i < len(input)-1; i++ {
+		if input[i]-input[i+1] == 32 || input[i+1]-input[i] == 32 {
+			return input[:i] + input[i+2:], true
+		}
+	}
+
+	return input, false
+}
+
+func fullyReactPolymer(input string) string {
+	reduction := true
+	for reduction {
+		input, reduction = reactPolymer(input)
+	}
+
+	return input
+}
+
 func (td dayEntry) PartOne(inputData string, updateChan chan []string) string {
-	return fmt.Sprintf(" -- Not Yet Implemented --")
+	return fmt.Sprintf("%v", len(fullyReactPolymer(inputData)))
+}
+
+func removeUnit(unit rune, input string) string {
+	input = strings.Replace(input, string(unit), "", -1)
+	input = strings.Replace(input, string(unit-32), "", -1)
+	return input
 }
 
 func (td dayEntry) PartTwo(inputData string, updateChan chan []string) string {
-	return fmt.Sprintf(" -- Not Yet Implemented --")
+	replacements := "abcdefghijklmnopqrstuvwxyz"
+
+	inputData = fullyReactPolymer(inputData)
+	min := -1
+	for _, char := range replacements {
+		newPolymer := removeUnit(char, inputData)
+
+		newPolymer = fullyReactPolymer(newPolymer)
+
+		if min == -1 || len(newPolymer) < min {
+			min = len(newPolymer)
+		}
+	}
+	// not 4, 1
+	return fmt.Sprintf("%v", min)
 }
--- 2018/Day201805/main_test.go
+++ 2018/Day201805/main_test.go
@@ -8,10 +8,29 @@ import (
 
 func Test_PartOne(t *testing.T) {
 	RegisterTestingT(t)
+
+	testPolymer := "dabAcCaCBAcCcaDA"
+	final := false
+	testPolymer, _ = reactPolymer(testPolymer)
+	Expect(testPolymer).Should(Equal("dabAaCBAcCcaDA"))
+
+	testPolymer, _ = reactPolymer(testPolymer)
+	Expect(testPolymer).Should(Equal("dabCBAcCcaDA"))
+
+	testPolymer, final = reactPolymer(testPolymer)
+	Expect(testPolymer).Should(Equal("dabCBAcaDA"))
+
+	testPolymer, final = reactPolymer(testPolymer)
+	Expect(testPolymer).Should(Equal("dabCBAcaDA"))
+	Expect(final).Should(Equal(false))
+
 }
 
 func Test_PartTwo(t *testing.T) {
 	RegisterTestingT(t)
+
+	Expect(removeUnit(97, "dabAcCaCBAcCcaDA")).Should(Equal("dbcCCBcCcD"))
+	Expect(fullyReactPolymer(removeUnit(97, "dabAcCaCBAcCcaDA"))).Should(Equal("dbCBcD"))
 }
 
 func Benchmark_BenchPartOne(b *testing.B) {
