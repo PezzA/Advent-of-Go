bf65a3d41badf1c15fe4707938b3e9a4e580707c
John Perrin <jperrin@travelrepublic.co.uk>
2018-12-07 06:50:50 +0000

    2018 day 7 complete

--- 2018/Day201806/main.go
+++ 2018/Day201806/main.go
@@ -1,15 +1,144 @@
 package Day201806
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
 
-// Entry holds wraps the data and runner interfaces for this puzzle
+	"github.com/pezza/advent-of-code/Common"
+)
+
 var Entry dayEntry
 
 type dayEntry bool
 
 func (td dayEntry) Describe() (int, int, string) {
-	return 2018, 6, "Getting the boilerplate in place"
+	return 2018, 6, "Chronal Coordinates"
 }
+
+// end of scaffolding
+
+type numberedPoint struct {
+	id string
+	common.Point
+}
+
+type infinitePlane map[common.Point]string
+
+func getData(inputData string) []numberedPoint {
+	points := make([]numberedPoint, 0)
+
+	startLabel := 65
+	for _, line := range strings.Split(inputData, "\n") {
+		point := numberedPoint{string(startLabel), common.Point{}}
+		fmt.Sscanf(line, "%d, %d", &point.X, &point.Y)
+		startLabel++
+		points = append(points, point)
+	}
+	return points
+}
+
+func getBounds(points []numberedPoint) (common.Point, common.Point) {
+	minX, minY, maxX, maxY := -1, -1, -1, -1
+
+	for _, point := range points {
+		if minX == -1 || point.X < minX {
+			minX = point.X
+		}
+
+		if maxX == -1 || point.X > maxX {
+			maxX = point.X
+		}
+
+		if minY == -1 || point.Y < minY {
+			minY = point.Y
+		}
+
+		if maxY == -1 || point.Y > maxX {
+			maxY = point.Y
+		}
+	}
+
+	return common.Point{minX, minY}, common.Point{maxX, maxY}
+}
+
+func getMDistance(source common.Point, target common.Point) int {
+	return common.Abs(source.X-target.X) + common.Abs(source.Y-target.Y)
+
+}
+
+func getNearestPoint(source common.Point, targets []numberedPoint) string {
+	distances := make(map[string]int)
+
+	for _, targetPoint := range targets {
+		distance := getMDistance(source, targetPoint.Point)
+		if distance == 0 {
+			return targetPoint.id
+		}
+		distances[targetPoint.id] = distance
+	}
+
+	min := -1
+
+	for _, v := range distances {
+		if min == -1 || v < min {
+			min = v
+		}
+	}
+
+	id := ""
+	for k, v := range distances {
+		if v == min {
+			if id != "" {
+				return "."
+			}
+			id = k
+		}
+	}
+
+	return strings.ToLower(id)
+}
+
+func getPlane(points []numberedPoint) infinitePlane {
+	min, max := getBounds(points)
+
+	plane := make(infinitePlane)
+
+	for x := min.X; x <= max.X; x++ {
+		for y := min.Y; y <= max.Y; y++ {
+			plane[common.Point{x, y}] = getNearestPoint(common.Point{x, y}, points)
+		}
+	}
+
+	return plane
+}
+
+func countBorders(min common.Point, max common.Point, points []numberedPoint) map[string]int {
+	counts := make(map[string]int)
+
+	for i := min.X; i <= max.X; i++ {
+		counts[strings.ToUpper(getNearestPoint(common.Point{i, min.Y}, points))]++
+		counts[strings.ToUpper(getNearestPoint(common.Point{i, max.Y}, points))]++
+	}
+
+	for i := min.Y + 1; i < max.Y; i++ {
+		counts[strings.ToUpper(getNearestPoint(common.Point{min.X, i}, points))]++
+		counts[strings.ToUpper(getNearestPoint(common.Point{max.X, i}, points))]++
+	}
+
+	return counts
+}
+
+func printPlane(min common.Point, max common.Point, plane infinitePlane) {
+	for x := min.X; x <= max.X; x++ {
+		for y := min.Y; y <= max.Y; y++ {
+			fmt.Print(plane[common.Point{y, x}])
+		}
+		fmt.Print("\n")
+	}
+}
+
+// method count the id's on the edges. if the number is going down for any, it's not infinite.  when we have no more reductions.  we have hit the upper bounds,
+// and can count the areas
 func (td dayEntry) PartOne(inputData string, updateChan chan []string) string {
 	return fmt.Sprintf(" -- Not Yet Implemented --")
 }
--- 2018/Day201806/main_test.go
+++ 2018/Day201806/main_test.go
@@ -1,15 +1,60 @@
 package Day201806
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/pezza/advent-of-code/Common"
 
 	. "github.com/onsi/gomega"
 )
 
+var testInput = `1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9`
+
 func Test_PartOne(t *testing.T) {
 	RegisterTestingT(t)
+
+	pointList := getData(testInput)
+
+	Expect(pointList[0]).Should(Equal(numberedPoint{"A", common.Point{1, 1}}))
+	Expect(pointList[1]).Should(Equal(numberedPoint{"B", common.Point{1, 6}}))
+
+	min, max := getBounds(pointList)
+	Expect(min).Should(Equal(common.Point{1, 1}))
+	Expect(max).Should(Equal(common.Point{8, 9}))
+
+	Expect(getMDistance(common.Point{-3, -3}, common.Point{3, 3})).Should(Equal(12))
+	Expect(getMDistance(common.Point{3, 3}, common.Point{-3, -3})).Should(Equal(12))
+
+	testPoint := common.Point{0, 0}
+	Expect(getNearestPoint(testPoint, pointList)).Should(Equal("a"))
+
+	testPoint = common.Point{1, 1}
+	Expect(getNearestPoint(testPoint, pointList)).Should(Equal("A"))
+	testPoint = common.Point{10, 10}
+	Expect(getNearestPoint(testPoint, pointList)).Should(Equal("f"))
+
+	testPoint = common.Point{5, 0}
+	Expect(getNearestPoint(testPoint, pointList)).Should(Equal("."))
+
 }
 
+func Test_PrintGrid(t *testing.T) {
+	pointList := getData(testInput)
+
+	min, max := getBounds(pointList)
+
+	//printPlane(min, max, getPlane(pointList))
+
+	fmt.Println(countBorders(min, max, pointList))
+	fmt.Println(countBorders(common.Point{min.X - 1, min.Y - 1}, common.Point{max.X + 1, max.Y + 1}, pointList))
+
+}
 func Test_PartTwo(t *testing.T) {
 	RegisterTestingT(t)
 }
