c01f64e48730b954dd539df52e0d3ba95415acb6
John Perrin <jperrin@travelrepublic.co.uk>
2018-12-08 13:04:12 +0000

    changes

--- 2018/Day201808/main.go
+++ 2018/Day201808/main.go
@@ -1,6 +1,10 @@
 package Day201808
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 // Entry holds wraps the data and runner interfaces for this puzzle
 var Entry dayEntry
@@ -8,8 +12,42 @@ var Entry dayEntry
 type dayEntry bool
 
 func (td dayEntry) Describe() (int, int, string) {
-	return 2018, 8, "Getting the boilerplate in place"
+	return 2018, 8, "Memory Maneuver"
 }
+
+//------------------------
+
+type node struct {
+	id         string
+	childNodes []node
+	metadata   []int
+}
+
+func getNode(input []int) node {
+	children := input[0]
+	metadataCount := input[1]
+
+	tail := len(input) - metadataCount
+	metadata := input[tail:]
+
+	newInput := input[2:tail]
+	node := node{"aa", []node{}, metadata}
+
+	for i := 0; i < children; i++ {
+		node.childNodes = append(node.childNodes, getNode(newInput))
+	}
+
+	return node
+}
+func getData(input string) []int {
+	vals := make([]int, 0)
+	for _, field := range strings.Fields(input) {
+		val, _ := strconv.Atoi(field)
+		vals = append(vals, val)
+	}
+	return vals
+}
+
 func (td dayEntry) PartOne(inputData string, updateChan chan []string) string {
 	return fmt.Sprintf(" -- Not Yet Implemented --")
 }
--- 2018/Day201808/main_test.go
+++ 2018/Day201808/main_test.go
@@ -6,6 +6,8 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+var testData = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
+
 func Test_PartOne(t *testing.T) {
 	RegisterTestingT(t)
 }
