58452502c6c78f6a3f960ee11b97a246dda64cba
John Perrin <jperrin@travelrepublic.co.uk>
2018-12-08 16:54:58 +0000

    2018 day 08 done

--- 2018/Day201808/main_test.go
+++ 2018/Day201808/main_test.go
@@ -1,6 +1,7 @@
 package Day201808
 
 import (
+	"fmt"
 	"testing"
 
 	. "github.com/onsi/gomega"
@@ -10,6 +11,14 @@ var testData = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
 
 func Test_PartOne(t *testing.T) {
 	RegisterTestingT(t)
+
+	vals := getData(Entry.PuzzleInput())
+	node, _ := getNode(vals)
+	mc := countMetaData(node)
+
+	fmt.Println(mc)
+
+	fmt.Println(advancedCount(node))
 }
 
 func Test_PartTwo(t *testing.T) {
