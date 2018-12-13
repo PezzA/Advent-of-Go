0280466e63eb5a1a393bd6a49dcb9eebaa8dc0b8
PezzA <john.d.perrin@gmail.com>
2018-12-13 06:49:39 +0000

    2018 13 partial

--- 2018/Day201813/main.go
+++ 2018/Day201813/main.go
@@ -1,13 +1,140 @@
 package Day201813
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 var Entry dayEntry
 
 type dayEntry bool
 
 func (td dayEntry) Describe() (int, int, string) {
-	return 0, 0, "Getting the boilerplate in place"
+	return 2018, 13, "Mine Cart Madness"
+}
+
+type cart struct {
+	direction int
+	x         int
+	y         int
+	state     int
+}
+
+func getData(input string) ([]string, []cart) {
+
+	b, err := ioutil.ReadFile("grid.txt") // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	input = string(b) // convert content to a 'string'
+
+	lines := make([]string, 0)
+	carts := make([]cart, 0)
+
+	for y, line := range strings.Split(input, "\n") {
+		lines = append(lines, line)
+
+		for x, char := range line {
+			if char == 60 {
+				carts = append(carts, cart{270, x, y, 0})
+			}
+			if char == 62 {
+				carts = append(carts, cart{90, x, y, 0})
+			}
+			if char == 118 {
+				carts = append(carts, cart{180, x, y, 0})
+			}
+			if char == 94 {
+				carts = append(carts, cart{0, x, y, 0})
+			}
+		}
+	}
+
+	return lines, carts
+}
+
+func (c cart) turnRight() cart {
+	c.direction += 90
+	if c.direction == 360 {
+		c.direction = 0
+	}
+	return c
+}
+
+func (c cart) turnLeft() cart {
+	c.direction -= 90
+	if c.direction == -90 {
+		c.direction = 270
+	}
+	return c
+}
+
+func moveCart(c cart, grid []string, carts []cart) (cart, bool) {
+	// get the location of where it would move next
+	switch c.direction {
+	case 0:
+		c.y--
+	case 180:
+		c.y++
+	case 270:
+		c.x--
+	case 90:
+		c.x++
+	}
+
+	// See if this will collide with another cart
+	for _, testCart := range carts {
+		if testCart.x == c.x && testCart.y == c.y {
+			return c, true
+		}
+	}
+
+	// see if we need to change direction
+
+	if c.x < 0 || c.y < 0 || c.y >= len(grid) || c.x >= len(grid[c.y]) || (c.y == 30 && c.direction == 270) {
+		fmt.Println("OOPS")
+	}
+
+	switch grid[c.y][c.x] {
+	case 47: // "/"
+		switch c.direction {
+		case 0:
+			c = c.turnRight()
+		case 90:
+			c = c.turnLeft()
+		case 180:
+			c = c.turnRight()
+		case 270:
+			c = c.turnLeft()
+		}
+
+	case 92: // "\"
+		switch c.direction {
+		case 0:
+			c = c.turnLeft()
+		case 90:
+			c = c.turnRight()
+		case 180:
+			c = c.turnLeft()
+		case 270:
+			c = c.turnRight()
+		}
+	case 43: // "+"
+		switch c.state {
+		case 0:
+			c.state = 1
+			c = c.turnLeft()
+		case 1:
+			c.state = 2
+		case 2:
+			c.state = 0
+			c = c.turnRight()
+		}
+	}
+
+	return c, false
 }
 
 func (td dayEntry) PartOne(inputData string, updateChan chan []string) string {
--- 2018/Day201813/main_test.go
+++ 2018/Day201813/main_test.go
@@ -3,11 +3,40 @@ package Day201813
 import (
 	"testing"
 
+	"fmt"
+
 	. "github.com/onsi/gomega"
+	"github.com/pezza/advent-of-code/Common"
 )
+
+var testData = `/---\
+|   |  /----\
+| /-+--+-\  |
+| | |  | ^  |
+\-+-/  \-+--/
+  \------/   `
 
 func Test_PartOne(t *testing.T) {
 	RegisterTestingT(t)
+
+	grid, carts := getData(Entry.PuzzleInput())
+
+	crashed := false
+	var point common.Point
+	for {
+		for index := range carts {
+			carts[index], crashed = moveCart(carts[index], grid, carts)
+			if crashed {
+				point = common.Point{carts[index].x, carts[index].y}
+			}
+		}
+
+		if crashed {
+			break
+		}
+	}
+
+	fmt.Println(point)
 }
 
 func Test_PartTwo(t *testing.T) {
