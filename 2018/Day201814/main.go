72f920e293a0df970de3eb3097b04522efdcbcc0
John Perrin <jperrin@travelrepublic.co.uk>
2018-12-14 13:31:55 +0000

    2018 14 complete

--- 2018/Day201814/main.go
+++ 2018/Day201814/main.go
@@ -1,6 +1,7 @@
 package Day201814
 
 import (
+	"container/list"
 	"fmt"
 	"strconv"
 )
@@ -18,24 +19,105 @@ func getData(input string) int {
 	return ret
 }
 
-func getRecipes(elf1 int, elf2 int, scores []int) (int, int, []int) {
-	sum := scores[elf1] + scores[elf2]
+func isResult(test string, scores *list.List) bool {
+	back := scores.Back()
+	check := ""
+
+	for i := 0; i < len(test); i++ {
+		check += strconv.Itoa(back.Value.(int))
+		back = back.Prev()
+	}
+
+	if check == test {
+		return true
+	}
+	return false
+}
+
+func getRecipes(elf1 *list.Element, elf2 *list.Element, scores *list.List, test string) (*list.Element, *list.Element, int) {
+	sum := elf1.Value.(int) + elf2.Value.(int)
 
 	for _, char := range strconv.Itoa(sum) {
 		newScore, _ := strconv.Atoi(string(char))
-		scores = append(scores, newScore)
+		scores.PushBack(newScore)
+
+		if test != "" && scores.Len() > 20 {
+			if isResult(test, scores) {
+				return nil, nil, scores.Len() - len(test)
+			}
+		}
 	}
 
-	elf1Pos := (elf1 + scores[elf1] + 1) % len(scores)
-	elf2Pos := (elf2 + scores[elf2] + 1) % len(scores)
+	loopVal := elf1.Value.(int) + 1
+	for i := 0; i < loopVal; i++ {
+		if elf1.Next() == nil {
+			elf1 = scores.Front()
+		} else {
+			elf1 = elf1.Next()
+		}
+	}
+	loopVal = elf2.Value.(int) + 1
+	for i := 0; i < loopVal; i++ {
+		if elf2.Next() == nil {
+			elf2 = scores.Front()
+		} else {
+			elf2 = elf2.Next()
+		}
+	}
 
-	return elf1Pos, elf2Pos, scores
+	return elf1, elf2, 0
 }
 
 func (td dayEntry) PartOne(inputData string, updateChan chan []string) string {
-	return fmt.Sprintf(" -- Not Yet Implemented --")
+
+	maxVal := getData(inputData)
+
+	scores := list.New()
+	elf1 := scores.PushBack(3)
+	elf2 := scores.PushBack(7)
+	output := ""
+	for {
+		elf1, elf2, _ = getRecipes(elf1, elf2, scores, "")
+
+		if scores.Len() > maxVal+10 {
+			pos := scores.Front()
+			for i := 0; i < maxVal; i++ {
+				pos = pos.Next()
+			}
+
+			for i := 0; i < 10; i++ {
+				output += strconv.Itoa(pos.Value.(int))
+				pos = pos.Next()
+			}
+			break
+		}
+	}
+
+	return output
+}
+
+func Reverse(s string) string {
+	var reverse string
+	for i := len(s) - 1; i >= 0; i-- {
+		reverse += string(s[i])
+	}
+	return reverse
 }
 
 func (td dayEntry) PartTwo(inputData string, updateChan chan []string) string {
-	return fmt.Sprintf(" -- Not Yet Implemented --")
+	scores := list.New()
+	elf1 := scores.PushBack(3)
+	elf2 := scores.PushBack(7)
+
+	reverseString := Reverse(inputData)
+	val := 0
+	for {
+		elf1, elf2, val = getRecipes(elf1, elf2, scores, reverseString)
+
+		if val != 0 {
+			break
+		}
+	}
+
+	return fmt.Sprintf("%v", val)
 }
--- 2018/Day201814/main_test.go
+++ 2018/Day201814/main_test.go
@@ -1,9 +1,8 @@
 package Day201814
 
 import (
+	"container/list"
 	"testing"
-
-	"fmt"
 
 	. "github.com/onsi/gomega"
 )
@@ -11,69 +10,29 @@ import (
 func Test_PartOne(t *testing.T) {
 	RegisterTestingT(t)
 
-	elf1, elf2, recipies := getRecipes(0, 1, []int{3, 7})
-	Expect(elf1).Should(Equal(0))
-	Expect(elf2).Should(Equal(1))
-	Expect(recipies).Should(Equal([]int{3, 7, 1, 0}))
+	scores := list.New()
+	elf1 := scores.PushBack(3)
+	elf2 := scores.PushBack(7)
 
-	elf1, elf2, recipies = getRecipes(elf1, elf2, recipies)
+	elf1, elf2 = getRecipes(elf1, elf2, scores, "")
 
-	Expect(elf1).Should(Equal(4))
-	Expect(elf2).Should(Equal(3))
-	Expect(recipies).Should(Equal([]int{3, 7, 1, 0, 1, 0}))
+	Expect(elf1.Value.(int)).Should(Equal(3))
+	Expect(elf2.Value.(int)).Should(Equal(7))
 
-	elf1, elf2, recipies = 0, 1, []int{3, 7}
+	elf1, elf2 = getRecipes(elf1, elf2, scores, "")
 
-	for len(recipies) < 320900 {
-		elf1, elf2, recipies = getRecipes(elf1, elf2, recipies)
-	}
+	Expect(elf1.Value.(int)).Should(Equal(1))
+	Expect(elf2.Value.(int)).Should(Equal(0))
 
-	fmt.Println(recipies[9:19])
-	fmt.Println(recipies[5:15])
-	fmt.Println(recipies[18:28])
-	fmt.Println(recipies[2018:2028])
-	fmt.Println(recipies[320851:320861])
+	scores = list.New()
+	elf1 = scores.PushBack(3)
+	elf2 = scores.PushBack(7)
+
 }
 
 func Test_PartTwo(t *testing.T) {
 	RegisterTestingT(t)
-
-	elf1, elf2, recipies := 0, 1, []int{3, 7}
-	check := []int{3, 2, 0, 8, 5, 1}
-	checkLen := len(check)
-
-	foundIndex := -1
-
-	prevSize := len(recipies)
-
-	for foundIndex == -1 {
-		elf1, elf2, recipies = getRecipes(elf1, elf2, recipies)
-
-		if len(recipies) > checkLen {
-
-			for i := len(recipies) - checkLen; i > len(recipies)-(prevSize-checkLen); i-- {
-				isSame := true
-				for index := range check {
-					if recipies[i+index] != check[index] {
-						isSame = false
-						break
-					}
-				}
-
-				if isSame {
-					foundIndex = i
-					break
-				}
-			}
-
-		}
-		prevSize = len(recipies)
-		if len(recipies)%10000 == 0 {
-			fmt.Println(len(recipies))
-		}
-	}
-
-	fmt.Println(foundIndex)
+	Entry.PartTwo(Entry.PuzzleInput(), nil)
 
 }
 
