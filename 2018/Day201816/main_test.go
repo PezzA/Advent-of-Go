d5640ee3bc8ba4c30490583fb3c18cef0cc80d79
John Perrin <jperrin@travelrepublic.co.uk>
2018-12-19 18:11:58 +0000

    changeS

--- 2018/Day201816/main_test.go
+++ 2018/Day201816/main_test.go
@@ -1,6 +1,7 @@
 package Day201816
 
 import (
+	"fmt"
 	"testing"
 
 	. "github.com/onsi/gomega"
@@ -8,6 +9,39 @@ import (
 
 func Test_PartOne(t *testing.T) {
 	RegisterTestingT(t)
+
+	ops := getOpCodes()
+
+	reg := make(registerSet)
+	reg[0] = 1
+	reg[1] = 1
+
+	Expect(ops["addr"].process(reg, 0, 1, 3)).Should(
+		Equal(registerSet{0: 1, 1: 1, 3: 2}))
+
+	tests, _ := getData(Entry.PuzzleInput())
+
+	Expect(tests[0].before).Should(Equal(registerSet{0: 1, 1: 1, 2: 0, 3: 1}))
+	Expect(tests[0].codes).Should(Equal([]int{0, 1, 0, 1}))
+	Expect(tests[0].after).Should(Equal(registerSet{0: 1, 1: 1, 2: 0, 3: 1}))
+
+	masterCount := 0
+	testCount := 0
+	for _, test := range tests {
+		count := 0
+
+		for _, v := range ops {
+			if v.process(test.before.deepCopy(), test.codes[1], test.codes[2], test.codes[3]).Same(test.after.deepCopy()) {
+				count++
+			}
+		}
+
+		if count >= 3 {
+			masterCount++
+		}
+		testCount++
+	}
+	fmt.Println(masterCount, testCount)
 }
 
 func Test_PartTwo(t *testing.T) {
