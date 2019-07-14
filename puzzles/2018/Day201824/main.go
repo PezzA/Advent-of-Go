1a4acdf56ee16d10669cb2a5d625208182f9bb7f
John Perrin <jperrin@travelrepublic.co.uk>
2019-07-14 17:39:54 +0100

    2018 day 24 stuff

--- puzzles/2018/Day201824/main.go
+++ puzzles/2018/Day201824/main.go
@@ -2,6 +2,9 @@ package Day201824
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 var Entry dayEntry
@@ -23,6 +26,97 @@ type group struct {
 	immunites  []string
 }
 
+var groupParser = regexp.MustCompile(`(?P<units>[0-9]*) units each with (?P<HP>[0-9]*) hit points( \((?P<mods>.*)*\))* with an attack that does (?P<atk>[0-9]*) (?P<atkType>\S*) damage at initiative (?P<init>[0-9]*)`)
+var groupParserNames = groupParser.SubexpNames()
+
+func parseAttributes(input string) ([]string, []string) {
+	input = strings.TrimSpace(input)
+	weaknesses, immunities := []string{}, []string{}
+
+	if input == "" {
+		return weaknesses, immunities
+	}
+
+	for _, line := range strings.Split(input, ";") {
+		words := strings.Fields(line)
+
+		if words[0] == "weak" {
+			weaknesses = words[2:]
+
+			for index := range weaknesses {
+				weaknesses[index] = strings.TrimSpace(strings.Replace(weaknesses[index], ",", "", -1))
+			}
+		}
+
+		if words[0] == "immune" {
+			immunities = words[2:]
+			for index := range immunities {
+				immunities[index] = strings.TrimSpace(strings.Replace(immunities[index], ",", "", -1))
+			}
+		}
+	}
+
+	return weaknesses, immunities
+}
+
+func getData(input string) ([]group, []group) {
+	immuneGroup := make([]group, 0)
+	infectionGroup := make([]group, 0)
+
+	lines := strings.Split(input, "\n")
+
+	isInfectionList := false
+	for index, line := range lines {
+		line = strings.TrimSpace(line)
+
+		if line == "" {
+			continue
+		}
+		if line == "Immune System:" {
+			isInfectionList = false
+			continue
+		}
+		if line == "Infection:" {
+			isInfectionList = true
+			continue
+		}
+
+		match := groupParser.FindStringSubmatch(line)
+		matchData := make(map[string]string, 0)
+
+		for index, matchName := range groupParserNames {
+			if index != 0 && matchName != "" {
+				matchData[matchName] = match[index]
+			}
+		}
+
+		weaknesses, immunities := parseAttributes(matchData["mods"])
+
+		units, _ := strconv.Atoi(matchData["units"])
+		hp, _ := strconv.Atoi(matchData["HP"])
+		atk, _ := strconv.Atoi(matchData["atk"])
+		init, _ := strconv.Atoi(matchData["init"])
+
+		newGroup := group{
+			id:         index,
+			units:      units,
+			hp:         hp,
+			attack:     atk,
+			initative:  init,
+			attackType: matchData["atkType"],
+			weaknesses: weaknesses,
+			immunites:  immunities,
+		}
+
+		if isInfectionList {
+			infectionGroup = append(infectionGroup, newGroup)
+		} else {
+			immuneGroup = append(immuneGroup, newGroup)
+		}
+	}
+
+	return immuneGroup, infectionGroup
+}
 func (td dayEntry) PartOne(inputData string, updateChan chan []string) string {
 	return fmt.Sprintf(" -- Not Yet Implemented --")
 }
--- puzzles/2018/Day201824/main_test.go
+++ puzzles/2018/Day201824/main_test.go
@@ -2,8 +2,24 @@ package Day201824
 
 import (
 	"testing"
+
+	. "github.com/onsi/gomega"
 )
 
+func Test_GetData(t *testing.T) {
+	RegisterTestingT(t)
+
+	immunes, infections := getData(Entry.PuzzleInput())
+
+	Expect(immunes[0]).Should(Equal(group{1, 4445, 10125, 20, 16, "cold", []string{}, []string{"radiation"}}))
+	Expect(immunes[2]).Should(Equal(group{3, 1767, 5757, 27, 4, "radiation", []string{"fire", "radiation"}, []string{}}))
+	Expect(immunes[9]).Should(Equal(group{10, 3091, 6684, 17, 19, "cold", []string{"radiation"}, []string{"slashing"}}))
+
+	Expect(infections[0]).Should(Equal(group{1, 1929, 13168, 13, 7, "fire", []string{"bludgeoning"}, []string{}}))
+	Expect(infections[2]).Should(Equal(group{3, 1767, 5757, 27, 4, "radiation", []string{"fire", "radiation"}, []string{}}))
+	Expect(infections[9]).Should(Equal(group{10, 3091, 6684, 17, 19, "cold", []string{"radiation"}, []string{"slashing"}}))
+
+}
 func Benchmark_BenchPartOne(b *testing.B) {
 	data := Entry.PuzzleInput()
 	for n := 0; n < b.N; n++ {
