72ce33cd1e867994c82b4b0efbcb389eedd2a5ba
John Perrin <jperrin@travelrepublic.co.uk>
2019-12-17 08:45:35 +0000

    2019 15 changes

--- 2019/Day201915/main.go
+++ 2019/Day201915/main.go
@@ -26,8 +26,6 @@ func runProgram(input string) {
 	maze[pos] = 0
 	crawl(pos, maze, in, out)
 
-	drawMaze(maze, pos)
-
 	for k, v := range maze {
 		if v == 2 {
 			fmt.Println(k.X, k.Y)
@@ -37,8 +35,6 @@ func runProgram(input string) {
 	counts := make(map[common.Point]int, 0)
 
 	pathMaze(maze, common.Point{-17, 12}, counts, 0)
-
-	drawcounts(counts)
 
 	min := 0
 	for _, v := range counts {
@@ -126,87 +122,6 @@ func crawl(pos common.Point, maze map[common.Point]int, in, out chan int64) {
 		<-out
 	}
 
-}
-
-func drawcounts(maze map[common.Point]int) {
-	minX, minY, maxX, maxY := common.MaxInt, common.MaxInt, common.MinInt, common.MinInt
-
-	for k, _ := range maze {
-		if k.X < minX {
-			minX = k.X
-		}
-
-		if k.Y < minY {
-			minY = k.Y
-		}
-
-		if k.X > maxX {
-			maxX = k.X
-		}
-
-		if k.Y > maxY {
-			maxY = k.Y
-		}
-	}
-
-	for y := minY; y < maxY+1; y++ {
-		for x := minX; x < maxX+1; x++ {
-
-			if val, ok := maze[common.Point{X: x, Y: y}]; ok {
-				fmt.Printf("%-4v", val)
-
-			} else {
-				fmt.Print("    ")
-			}
-		}
-		fmt.Print("\n")
-	}
-}
-
-func drawMaze(maze map[common.Point]int, rob common.Point) {
-	minX, minY, maxX, maxY := common.MaxInt, common.MaxInt, common.MinInt, common.MinInt
-
-	for k, _ := range maze {
-		if k.X < minX {
-			minX = k.X
-		}
-
-		if k.Y < minY {
-			minY = k.Y
-		}
-
-		if k.X > maxX {
-			maxX = k.X
-		}
-
-		if k.Y > maxY {
-			maxY = k.Y
-		}
-	}
-
-	for y := minY; y < maxY+1; y++ {
-		for x := minX; x < maxX+1; x++ {
-
-			if x == rob.X && y == rob.Y {
-				fmt.Print("D")
-				continue
-			}
-
-			if val, ok := maze[common.Point{X: x, Y: y}]; ok {
-				switch val {
-				case 0:
-					fmt.Print(" ")
-				case 1:
-					fmt.Print("#")
-				case 2:
-					fmt.Print("A")
-				}
-			} else {
-				fmt.Print("_")
-			}
-		}
-		fmt.Print("\n")
-	}
 }
 
 func (td dayEntry) PartOne(inputData string, updateChan chan []string) string {
---
+++ 2019/Day201915/output.go
@@ -0,0 +1,88 @@
+package Day201915
+
+import (
+	"fmt"
+
+	"github.com/pezza/advent-of-code/common"
+)
+
+func drawcounts(maze map[common.Point]int) {
+	minX, minY, maxX, maxY := common.MaxInt, common.MaxInt, common.MinInt, common.MinInt
+
+	for k, _ := range maze {
+		if k.X < minX {
+			minX = k.X
+		}
+
+		if k.Y < minY {
+			minY = k.Y
+		}
+
+		if k.X > maxX {
+			maxX = k.X
+		}
+
+		if k.Y > maxY {
+			maxY = k.Y
+		}
+	}
+
+	for y := minY; y < maxY+1; y++ {
+		for x := minX; x < maxX+1; x++ {
+
+			if val, ok := maze[common.Point{X: x, Y: y}]; ok {
+				fmt.Printf("%-4v", val)
+
+			} else {
+				fmt.Print("    ")
+			}
+		}
+		fmt.Print("\n")
+	}
+}
+
+func drawMaze(maze map[common.Point]int, rob common.Point) {
+	minX, minY, maxX, maxY := common.MaxInt, common.MaxInt, common.MinInt, common.MinInt
+
+	for k, _ := range maze {
+		if k.X < minX {
+			minX = k.X
+		}
+
+		if k.Y < minY {
+			minY = k.Y
+		}
+
+		if k.X > maxX {
+			maxX = k.X
+		}
+
+		if k.Y > maxY {
+			maxY = k.Y
+		}
+	}
+
+	for y := minY; y < maxY+1; y++ {
+		for x := minX; x < maxX+1; x++ {
+
+			if x == rob.X && y == rob.Y {
+				fmt.Print("D")
+				continue
+			}
+
+			if val, ok := maze[common.Point{X: x, Y: y}]; ok {
+				switch val {
+				case 0:
+					fmt.Print(" ")
+				case 1:
+					fmt.Print("#")
+				case 2:
+					fmt.Print("A")
+				}
+			} else {
+				fmt.Print("_")
+			}
+		}
+		fmt.Print("\n")
+	}
+}
