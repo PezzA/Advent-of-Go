753daca0dd9d39a6146b428ab17f7fb834cecabd
John Perrin <john.d.perrin@gmail.com>
2020-12-06 05:31:25 +0000

    2020 day 06 complete

--- 2020/Day202006/main.go
+++ 2020/Day202006/main.go
@@ -1,11 +1,88 @@
 package Day202006
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+type group []string
+
+func getData(input string) []group {
+	data := make([]group, 0)
+
+	current := make(group, 0)
+	for _, line := range strings.Split(input, "\n") {
+
+		if line == "" {
+			data = append(data, current)
+			current = make(group, 0)
+			continue
+		}
+		current = append(current, line)
+	}
+	data = append(data, current)
+
+	return data
+}
+
+func (g group) getDistinctYes() int {
+	questions := make(map[rune]int, 0)
+
+	for _, passenger := range g {
+		for _, question := range passenger {
+
+			if _, ok := questions[question]; ok {
+				questions[question]++
+			} else {
+				questions[question] = 1
+			}
+		}
+	}
+
+	return len(questions)
+}
+
+func (g group) getCollectiveYes() int {
+	questions := make(map[rune]int, 0)
+
+	for _, passenger := range g {
+		for _, question := range passenger {
+
+			if _, ok := questions[question]; ok {
+				questions[question]++
+			} else {
+				questions[question] = 1
+			}
+		}
+	}
+
+	collective := 0
+
+	for _, v := range questions {
+		if v == len(g) {
+			collective++
+		}
+	}
+
+	return collective
+}
 
 func (td dayEntry) PartOne(inputData string, updateChan chan []string) string {
-	return fmt.Sprintf("%v", " -- Not Yet Implemented --")
+	total := 0
+
+	for _, g := range getData(inputData) {
+		total += g.getDistinctYes()
+	}
+
+	return fmt.Sprintf("%v", total)
 }
 
 func (td dayEntry) PartTwo(inputData string, updateChan chan []string) string {
-	return fmt.Sprintf("%v", " -- Not Yet Implemented --")
+	total := 0
+
+	for _, g := range getData(inputData) {
+		total += g.getCollectiveYes()
+	}
+
+	return fmt.Sprintf("%v", total)
 }
--- 2020/Day202006/main_test.go
+++ 2020/Day202006/main_test.go
@@ -1,6 +1,7 @@
 package Day202006
 
 import (
+	"fmt"
 	"testing"
 
 	. "github.com/onsi/gomega"
@@ -9,16 +10,59 @@ import (
 func Test_ReadData(t *testing.T) {
 	RegisterTestingT(t)
 
+	data := getData(Entry.PuzzleInput())
+
+	Expect(len(data[0])).Should(Equal(5))
+
+	Expect(len(data[len(data)-1])).Should(Equal(4))
+}
+
+func Test_DistinctYes(t *testing.T) {
+	RegisterTestingT(t)
+
+	data := getData(`abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b`)
+
+	for _, g := range data {
+		fmt.Println(g.getDistinctYes())
+	}
 }
 
 func Test_PartOne(t *testing.T) {
 	RegisterTestingT(t)
 
+	total := 0
+
+	for _, g := range getData(Entry.PuzzleInput()) {
+		total += g.getDistinctYes()
+	}
+
+	fmt.Println(total)
 }
 
 func Test_PartTwo(t *testing.T) {
 	RegisterTestingT(t)
 
+	total := 0
+
+	for _, g := range getData(Entry.PuzzleInput()) {
+		total += g.getCollectiveYes()
+	}
+
+	fmt.Println(total)
 }
 
 func Benchmark_BenchPartOne(b *testing.B) {
