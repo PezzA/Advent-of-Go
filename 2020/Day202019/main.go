bdd9403ce00fd94cd68db914f1a24c156643dd94
John Perrin <john.d.perrin@gmail.com>
2020-12-19 13:37:52 +0000

    2020 day 19 complete

--- 2020/Day202019/main.go
+++ 2020/Day202019/main.go
@@ -1,11 +1,126 @@
 package Day202019
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type expression struct {
+	char     string
+	subRules [][]int
+}
+
+func getData(input string) (map[int]expression, []string, int, int) {
+	lines := strings.Split(input, "\n")
+
+	expressions := make(map[int]expression, 0)
+	tests := make([]string, 0)
+
+	insA, insB := 0, 0
+	processingTests := false
+	for _, line := range lines {
+		if line == "" {
+			processingTests = true
+			continue
+		}
+
+		if !processingTests {
+			bits := strings.Split(line, ":")
+			ins, _ := strconv.Atoi(bits[0])
+
+			if strings.TrimSpace(bits[1])[1] == 98 {
+				expressions[ins] = expression{"b", [][]int{}}
+				insB = ins
+				continue
+			}
+
+			if strings.TrimSpace(bits[1])[1] == 97 {
+				expressions[ins] = expression{"a", [][]int{}}
+				insA = ins
+				continue
+			}
+
+			subRules := strings.Split(bits[1], "|")
+
+			subRuleSlice := make([][]int, len(subRules))
+			for r, rule := range subRules {
+				ruleBits := strings.Split(strings.TrimSpace(rule), " ")
+
+				ruleSlice := make([]int, len(ruleBits))
+				for i := range ruleBits {
+					ruleSlice[i], _ = strconv.Atoi(ruleBits[i])
+				}
+
+				subRuleSlice[r] = ruleSlice
+			}
+
+			expressions[ins] = expression{"", subRuleSlice}
+
+			continue
+		}
+
+		tests = append(tests, line)
+	}
+
+	return expressions, tests, insA, insB
+}
+
+func (e expression) getText(insA, insB int, exps map[int]expression) string {
+	val := ""
+
+	for _, sr := range e.subRules {
+
+		if val != "" {
+			val += "|"
+		}
+
+		for _, r := range sr {
+			if r == insA || r == insB {
+				val += exps[r].char
+				continue
+			}
+			val += "(" + exps[r].getText(insA, insB, exps) + ")"
+		}
+	}
+
+	return val
+}
 
 func (td dayEntry) PartOne(inputData string, updateChan chan []string) string {
-	return fmt.Sprintf("%v", " -- Not Yet Implemented --")
+	exp, tests, aIns, bIns := getData(inputData)
+
+	re := regexp.MustCompile("^" + exp[0].getText(aIns, bIns, exp) + "$")
+	tot := 0
+
+	for _, t := range tests {
+		if re.MatchString(t) {
+			tot++
+		}
+	}
+
+	return fmt.Sprintf("%v", tot)
 }
 
 func (td dayEntry) PartTwo(inputData string, updateChan chan []string) string {
-	return fmt.Sprintf("%v", " -- Not Yet Implemented --")
+	exp, tests, aIns, bIns := getData(inputData)
+
+	tot := 0
+
+	exp42 := exp[42].getText(aIns, bIns, exp)
+	exp31 := exp[31].getText(aIns, bIns, exp)
+
+	// 6 is arbitrary based on how long the input data is.
+	for i := 1; i < 6; i++ {
+		expression := fmt.Sprintf("^(%v)+(%v){%d}(%v){%d}$", exp42, exp42, i, exp31, i)
+		re := regexp.MustCompile(expression)
+		for _, t := range tests {
+			if re.MatchString(t) {
+				tot++
+			}
+		}
+	}
+
+	return fmt.Sprintf("%v", tot)
 }
--- 2020/Day202019/main_test.go
+++ 2020/Day202019/main_test.go
@@ -1,6 +1,8 @@
 package Day202019
 
 import (
+	"fmt"
+	"regexp"
 	"testing"
 
 	. "github.com/onsi/gomega"
@@ -8,6 +10,39 @@ import (
 
 func Test_ReadData(t *testing.T) {
 	RegisterTestingT(t)
+	exp, tests, _, _ := getData(Entry.PuzzleInput())
+	Expect(exp[110].char).Should(Equal("b"))
+	Expect(tests[0]).Should(Equal("aaababaaaaaabbbaaaabbaaa"))
+
+	fmt.Println(exp[0])
+	fmt.Println(exp[8])
+	fmt.Println(exp[11])
+}
+
+func Test_GetText(t *testing.T) {
+	RegisterTestingT(t)
+
+	exp, tests, aIns, bIns := getData(Entry.PuzzleInput())
+
+	fmt.Println(exp[0])
+	fmt.Println(exp[8])
+	fmt.Println(exp[11])
+	exp42 := exp[42].getText(aIns, bIns, exp)
+	exp31 := exp[31].getText(aIns, bIns, exp)
+
+	tot := 0
+
+	for i := 1; i < 20; i++ {
+		expression := fmt.Sprintf("^(%v)+(%v){%d}(%v){%d}$", exp42, exp42, i, exp31, i)
+		re := regexp.MustCompile(expression)
+		for _, t := range tests {
+			if re.MatchString(t) {
+				tot++
+			}
+		}
+	}
+
+	Expect(tot).Should(Equal(2))
 
 }
 
@@ -17,6 +52,13 @@ func Test_PartOne(t *testing.T) {
 }
 
 func Test_PartTwo(t *testing.T) {
+
+	// 480 too high
+	// 433 too high
+
+	// 424 ~
+
+	// 299 too low
 	RegisterTestingT(t)
 
 }
