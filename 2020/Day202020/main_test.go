350c87df2a38f1d589db839cd7322f9e31dbd4c6
John Perrin <john.d.perrin@gmail.com>
2020-12-20 13:51:13 +0000

    2020 day 20 complete

--- 2020/Day202020/main_test.go
+++ 2020/Day202020/main_test.go
@@ -16,7 +16,6 @@ func Test_ReadData(t *testing.T) {
 	Expect(tiles[1].id).Should(Equal(3413))
 
 	fmt.Print(len(tiles))
-
 }
 
 func Test_Flip(t *testing.T) {
@@ -26,11 +25,11 @@ func Test_Flip(t *testing.T) {
 
 	tiles[0].printTile()
 	fmt.Println()
-	printData(flipLeftRight(tiles[0].active))
+	tiles[0].grid.flipLeftRight().printData()
 	fmt.Println()
-	printData(flipTopBottom(tiles[0].active))
+	tiles[0].grid.flipTopBottom().printData()
 	fmt.Println()
-	printData(rotate(tiles[0].active))
+	tiles[0].grid.rotate().printData()
 }
 
 func Test_matchingSides(t *testing.T) {
@@ -39,12 +38,10 @@ func Test_matchingSides(t *testing.T) {
 	tiles := getData(Entry.PuzzleInput())
 
 	tot := 1
-	for _, tile := range tiles {
-		fmt.Println(tile.matchingSides(tiles))
-		if tile.matchingSides(tiles) == 2 {
-			tot *= tile.id
+	for _, t := range tiles {
+		if len(t.matchingSides(tiles, [][]tile{})) == 2 {
+			tot *= t.id
 		}
-
 	}
 
 	fmt.Println(tot)
@@ -52,14 +49,54 @@ func Test_matchingSides(t *testing.T) {
 
 func Test_PartOne(t *testing.T) {
 	RegisterTestingT(t)
-
+	tiles := getData(Entry.PuzzleInput())
+	tot := 1
+	// don't have to put it all together, just need the corners
+	for _, t := range tiles {
+		if len(t.matchingSides(tiles, [][]tile{})) == 2 {
+			fmt.Println(t.id)
+			tot *= t.id
+		}
+	}
+	fmt.Println(tot)
 }
 
 func Test_PartTwo(t *testing.T) {
 	RegisterTestingT(t)
 
+	tiles := getData(Entry.PuzzleInput())
+
+	completedJig := solveJigSaw(tiles)
+
+	for _, row := range completedJig {
+		for _, tile := range row {
+			fmt.Print(tile.id, " ")
+		}
+		fmt.Println()
+	}
 }
 
+func Test_StripBorder(t *testing.T) {
+	tiles := getData(Entry.PuzzleInput())
+
+	completedJig := solveJigSaw(tiles)
+
+	completedJig[0][0].grid.stripBorder().printData()
+
+	grid := stitchTogether(completedJig)
+	mask := getMonsterMask()
+	cmp := grid.copyData()
+	for i := 0; i < 4; i++ {
+		cmp = cmp.rotate()
+
+		fmt.Println(searchGrid(mask, cmp))
+		cmpLR := cmp.flipLeftRight()
+		fmt.Println(searchGrid(mask, cmpLR))
+		cmpTB := cmp.flipTopBottom()
+		fmt.Println(searchGrid(mask, cmpTB))
+
+	}
+}
 func Benchmark_BenchPartOne(b *testing.B) {
 	data := Entry.PuzzleInput()
 	for n := 0; n < b.N; n++ {
