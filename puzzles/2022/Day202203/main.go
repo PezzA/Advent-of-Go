d8b9186b922b5d9b839c330a6e789b192d5e0d8f
PezzA <john.d.perrin@gmail.com>
2022-12-03 08:33:02 +0000

    2022 day 3

--- puzzles/2022/Day202203/main.go
+++ puzzles/2022/Day202203/main.go
@@ -2,14 +2,84 @@ package Day202203
 
 import (
 	"fmt"
-
-	"github.com/pezza/advent-of-code/puzzles/common"
+	"strings"
 )
 
+type rucksack string
+
+func getData(input string) []rucksack {
+	lines := strings.Split(input, "\n")
+
+	data := []rucksack{}
+
+	for _, line := range lines {
+		data = append(data, rucksack(line))
+	}
+
+	return data
+}
+
+func (r rucksack) getCommonItem() rune {
+	cmp1, cmp2 := splitInHalf(string(r))
+
+	for _, char1 := range cmp1 {
+		for _, char2 := range cmp2 {
+			if char1 == char2 {
+				return char1
+			}
+		}
+	}
+	return rune(0)
+}
+
+func getItemPriority(input rune) int {
+	val := int(input)
+	if val >= 97 {
+		return val - 96
+	}
+
+	return val - 38
+}
+
+func getCommonItemType(r1, r2, r3 rucksack) rune {
+	for _, char1 := range string(r1) {
+		for _, char2 := range string(r2) {
+			if char1 == char2 {
+				for _, char3 := range string(r3) {
+					if char1 == char3 {
+						return char1
+					}
+				}
+			}
+		}
+	}
+
+	return rune(0)
+}
+
+// assumes always even lengthed input
+func splitInHalf(input string) (string, string) {
+	pos := len(input) / 2
+	return input[:pos], input[pos:]
+}
+
 func (td dayEntry) PartOne(inputData string, updateChan chan []string) string {
-	return fmt.Sprint(common.NOT_IMPLEMENTED)
+	data := getData(inputData)
+	total := 0
+
+	for _, rs := range data {
+		total += getItemPriority(rs.getCommonItem())
+	}
+
+	return fmt.Sprint(total)
 }
 
 func (td dayEntry) PartTwo(inputData string, updateChan chan []string) string {
-	return fmt.Sprint(common.NOT_IMPLEMENTED)
+	data := getData(inputData)
+	total := 0
+
+	for i := 0; i < len(data); i += 3 {
+		total += getItemPriority(getCommonItemType(data[i], data[i+1], data[i+2]))
+	}
+	return fmt.Sprint(total)
 }
--- puzzles/2022/Day202203/main_test.go
+++ puzzles/2022/Day202203/main_test.go
@@ -4,24 +4,74 @@ import (
 	"testing"
 
 	. "github.com/onsi/gomega"
-	"github.com/pezza/advent-of-code/puzzles/common"
 )
 
 func Test_ReadData(t *testing.T) {
 	RegisterTestingT(t)
 
+	data := getData(Entry.PuzzleInput())
+
+	Expect(data[0]).Should(Equal(rucksack("FqdWDFppHWhmwwzdjvjTRTznjdMv")))
+}
+
+func Test_GetCommonItem(t *testing.T) {
+	RegisterTestingT(t)
+
+	r := rucksack("vJrwpWtwJgWrhcsFMMfFFhFp")
+
+	Expect(r.getCommonItem()).Should(Equal(rune(112)))
+
+}
+
+func Test_GetPriority(t *testing.T) {
+	RegisterTestingT(t)
+
+	Expect(getItemPriority(rune(97))).Should(Equal(1))
+	Expect(getItemPriority(rune(65))).Should(Equal(27))
+}
+
+func Test_SplitInHalf(t *testing.T) {
+	RegisterTestingT(t)
+
+	cmp1, cmp2 := splitInHalf("vJrwpWtwJgWrhcsFMMfFFhFp")
+
+	Expect(cmp1).Should(Equal("vJrwpWtwJgWr"))
+	Expect(cmp2).Should(Equal("hcsFMMfFFhFp"))
+}
+
+func Test_GetBadge(t *testing.T) {
+	RegisterTestingT(t)
+
+	data := getData(`vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg`)
+
+	Expect(getCommonItemType(data[0], data[1], data[2])).Should(Equal(rune(114)))
+}
+
+func Test_PartOneCases(t *testing.T) {
+	RegisterTestingT(t)
+
+	data := getData(`vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`)
+
+	Expect(getItemPriority(data[0].getCommonItem())).Should(Equal(16))
 }
 
 func Test_PartOne(t *testing.T) {
 	RegisterTestingT(t)
-
-	Expect(Entry.PartOne(Entry.PuzzleInput(), nil)).Should(Equal(common.NOT_IMPLEMENTED))
+	// not 8111
+	Expect(Entry.PartOne(Entry.PuzzleInput(), nil)).Should(Equal("8243"))
 }
 
 func Test_PartTwo(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(Entry.PartTwo(Entry.PuzzleInput(), nil)).Should(Equal(common.NOT_IMPLEMENTED))
+	Expect(Entry.PartTwo(Entry.PuzzleInput(), nil)).Should(Equal("2631"))
 }
 
 func Benchmark_BenchPartOne(b *testing.B) {
