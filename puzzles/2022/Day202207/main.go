4ac880e9b4479167da40084aac6202d01eb1622e
PezzA <john.d.perrin@gmail.com>
2022-12-09 20:00:31 +0000

    2022 day 07

--- puzzles/2022/Day202207/main.go
+++ puzzles/2022/Day202207/main.go
@@ -2,14 +2,127 @@ package Day202207
 
 import (
 	"fmt"
-
-	"github.com/pezza/advent-of-code/puzzles/common"
+	"math"
+	"strconv"
+	"strings"
 )
 
+type directory struct {
+	dirs   map[string]*directory
+	files  map[string]int
+	parent *directory
+	size   int // size in bytes of the directory
+}
+
+func newDir(parent *directory) *directory {
+	blankDirs := make(map[string]*directory, 0)
+	files := make(map[string]int)
+	return &directory{
+		dirs:   blankDirs,
+		files:  files,
+		parent: parent,
+		size:   0,
+	}
+}
+
+func getData(input string) *directory {
+	lines := strings.Split(input, "\n")
+
+	root := newDir(nil)
+
+	currDir := root
+	for _, line := range lines {
+		bits := strings.Split(line, " ")
+
+		switch bits[0] {
+		case "$":
+			switch bits[1] {
+			case "cd":
+				switch bits[2] {
+				case "..":
+					currDir.parent.size += currDir.size
+					currDir = currDir.parent
+				case "/":
+					currDir = root
+				default:
+					currDir = currDir.dirs[bits[2]]
+				}
+			case "ls":
+				continue
+			}
+		case "dir":
+			currDir.dirs[bits[1]] = newDir(currDir)
+		default:
+			size, _ := strconv.Atoi(bits[0])
+			currDir.files[bits[1]] = size
+			currDir.size += size
+		}
+	}
+
+	return root
+}
+
+func calcFolderSize(fs *directory) int {
+	size := 0
+
+	for _, fileSize := range fs.files {
+		size += fileSize
+	}
+
+	for _, dir := range fs.dirs {
+		size += calcFolderSize(dir)
+	}
+
+	fs.size = size
+
+	return size
+}
+
+func getFoldersUnderSizeLimit(fs *directory, limit int) int {
+	count := 0
+
+	if fs.size < limit {
+		count += fs.size
+	}
+
+	for _, dir := range fs.dirs {
+		count += getFoldersUnderSizeLimit(dir, limit)
+	}
+
+	return count
+}
+
 func (td dayEntry) PartOne(inputData string, updateChan chan []string) string {
-	return fmt.Sprint(common.NOT_IMPLEMENTED)
+	fs := getData(inputData)
+	calcFolderSize(fs)
+
+	return fmt.Sprint(getFoldersUnderSizeLimit(fs, 100000))
+}
+
+func getSmallestToReclaim(fs *directory, reclaimThreshold int, currentSmallest int) int {
+
+	if fs.size >= reclaimThreshold && fs.size < currentSmallest {
+		currentSmallest = fs.size
+	}
+
+	for _, dir := range fs.dirs {
+		dirSize := getSmallestToReclaim(dir, reclaimThreshold, currentSmallest)
+
+		if dirSize < currentSmallest {
+			currentSmallest = dirSize
+		}
+
+	}
+	return currentSmallest
 }
 
 func (td dayEntry) PartTwo(inputData string, updateChan chan []string) string {
-	return fmt.Sprint(common.NOT_IMPLEMENTED)
+	fs := getData(inputData)
+	totalSize := calcFolderSize(fs)
+	maxSize, spaceNeeded := 70000000, 30000000
+	// 1735494
+	sizeToReclaim := spaceNeeded - (maxSize - totalSize)
+
+	smallestReclaimDirSize := getSmallestToReclaim(fs, sizeToReclaim, math.MaxInt32)
+	return fmt.Sprint(smallestReclaimDirSize)
 }
--- puzzles/2022/Day202207/main_test.go
+++ puzzles/2022/Day202207/main_test.go
@@ -1,27 +1,83 @@
 package Day202207
 
 import (
+	"math"
 	"testing"
 
 	. "github.com/onsi/gomega"
-	"github.com/pezza/advent-of-code/puzzles/common"
 )
+
+var sampleInput string = `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k`
 
 func Test_ReadData(t *testing.T) {
 	RegisterTestingT(t)
 
+	fs := getData(sampleInput)
+
+	Expect(len(fs.dirs)).Should(Equal(2))
+	Expect(len(fs.files)).Should(Equal(2))
+}
+
+func Test_WalkTree(t *testing.T) {
+	RegisterTestingT(t)
+
+	fs := getData(sampleInput)
+
+	calcFolderSize(fs)
+
+	Expect(fs.size).Should(Equal(48381165))
+
+	size := getFoldersUnderSizeLimit(fs, 100000)
+
+	Expect(size).Should(Equal(95437))
+}
+
+func Test_Smallest(t *testing.T) {
+
+	RegisterTestingT(t)
+
+	fs := getData(sampleInput)
+
+	totalSize := calcFolderSize(fs)
+	maxSize, spaceNeeded := 70000000, 30000000
+	// 1735494
+	sizeToReclaim := spaceNeeded - (maxSize - totalSize)
+
+	smallestReclaimDirSize := getSmallestToReclaim(fs, sizeToReclaim, math.MaxInt32)
+
+	Expect(smallestReclaimDirSize).Should(Equal(24933642))
 }
 
 func Test_PartOne(t *testing.T) {
 	RegisterTestingT(t)
-
-	Expect(Entry.PartOne(Entry.PuzzleInput(), nil)).Should(Equal(common.NOT_IMPLEMENTED))
+	Expect(Entry.PartOne(Entry.PuzzleInput(), nil)).Should(Equal("1648397"))
 }
 
 func Test_PartTwo(t *testing.T) {
 	RegisterTestingT(t)
-
-	Expect(Entry.PartTwo(Entry.PuzzleInput(), nil)).Should(Equal(common.NOT_IMPLEMENTED))
+	Expect(Entry.PartTwo(Entry.PuzzleInput(), nil)).Should(Equal("1815525"))
 }
 
 func Benchmark_BenchPartOne(b *testing.B) {
