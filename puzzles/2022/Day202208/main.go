341992d92aacfe947d908a5752eb2cd030657b2a
PezzA <john.d.perrin@gmail.com>
2022-12-10 09:45:52 +0000

    2022 day 08

--- puzzles/2022/Day202208/main.go
+++ puzzles/2022/Day202208/main.go
@@ -1,11 +1,156 @@
-package Day202208 
+package Day202208
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pezza/advent-of-code/puzzles/common"
+)
+
+// assumes will always contains digits 0-9
+func getData(input string) ([][]int, int, int) {
+	lines := strings.Split(input, "\n")
+
+	grid := make([][]int, len(lines))
+
+	for lineIndex, line := range lines {
+		grid[lineIndex] = make([]int, 0)
+
+		for _, char := range line {
+			// convert ascii code for number to number itself
+			grid[lineIndex] = append(grid[lineIndex], int(char)-48)
+		}
+	}
+
+	return grid, len(grid[0]), len(grid)
+}
+
+func isTreeVisible(grid [][]int, point common.Point) bool {
+	if point.X == 0 || point.Y == 0 || point.X == len(grid)-1 || point.Y == len(grid[0])-1 {
+		return true
+	}
+
+	if canViewFromUp(grid, point) ||
+		canViewFromDown(grid, point) ||
+		canViewFromLeft(grid, point) ||
+		canViewFromRight(grid, point) {
+		return true
+	}
+
+	return false
+}
+
+func getScenicScore(grid [][]int, point common.Point) int {
+	height := grid[point.Y][point.X]
+	seeUp, seeDown, seeLeft, seeRight := 0, 0, 0, 0
+	// how many up
+	for i := point.Y - 1; i >= 0; i-- {
+		seeUp++
+		if grid[i][point.X] >= height {
+			break
+		}
+	}
+
+	// how many down
+	for i := point.Y + 1; i < len(grid); i++ {
+		seeDown++
+		if grid[i][point.X] >= height {
+			break
+		}
+	}
+
+	// how many left
+	for i := point.X - 1; i >= 0; i-- {
+		seeLeft++
+		if grid[point.Y][i] >= height {
+			break
+		}
+	}
+	// how many right
+	for i := point.X + 1; i < len(grid[0]); i++ {
+		seeRight++
+		if grid[point.Y][i] >= height {
+			break
+		}
+	}
+	return seeUp * seeDown * seeLeft * seeRight
+}
+
+func canViewFromUp(grid [][]int, point common.Point) bool {
+	height := grid[point.Y][point.X]
+
+	for i := point.Y - 1; i >= 0; i-- {
+		if grid[i][point.X] >= height {
+			return false
+		}
+	}
+	return true
+}
+
+func canViewFromDown(grid [][]int, point common.Point) bool {
+	height := grid[point.Y][point.X]
+
+	for i := point.Y + 1; i < len(grid); i++ {
+		if grid[i][point.X] >= height {
+			return false
+		}
+	}
+	return true
+}
+
+func canViewFromLeft(grid [][]int, point common.Point) bool {
+	height := grid[point.Y][point.X]
+
+	for i := point.X - 1; i >= 0; i-- {
+		if grid[point.Y][i] >= height {
+			return false
+		}
+	}
+	return true
+}
+
+func canViewFromRight(grid [][]int, point common.Point) bool {
+	height := grid[point.Y][point.X]
+
+	for i := point.X + 1; i < len(grid[0]); i++ {
+		if grid[point.Y][i] >= height {
+			return false
+		}
+	}
+	return true
+}
+
+func getTreeCount(grid [][]int) int {
+	count := 0
+	for x := 0; x < len(grid[0]); x++ {
+		for y := 0; y < len(grid); y++ {
+			if isTreeVisible(grid, common.Point{x, y}) {
+				count++
+			}
+		}
+	}
+	return count
+}
 
 func (td dayEntry) PartOne(inputData string, updateChan chan []string) string {
-	return fmt.Sprintf("%v", " -- Not Yet Implemented --")
+	grid, _, _ := getData(inputData)
+	return fmt.Sprint(getTreeCount(grid))
 }
 
 func (td dayEntry) PartTwo(inputData string, updateChan chan []string) string {
-	return fmt.Sprintf("%v", " -- Not Yet Implemented --")
+	grid, _, _ := getData(inputData)
+
+	bestScenicScore := 0
+
+	for x := 0; x < len(grid[0]); x++ {
+		for y := 0; y < len(grid); y++ {
+			score := getScenicScore(grid, common.Point{x, y})
+
+			if score > bestScenicScore {
+				bestScenicScore = score
+			}
+		}
+	}
+
+	return fmt.Sprint(bestScenicScore)
 }
--- puzzles/2022/Day202208/main_test.go
+++ puzzles/2022/Day202208/main_test.go
@@ -1,24 +1,64 @@
-package Day202208 
+package Day202208
 
 import (
 	"testing"
 
 	. "github.com/onsi/gomega"
+	"github.com/pezza/advent-of-code/puzzles/common"
 )
+
+const testData string = `30373
+25512
+65332
+33549
+35390`
 
 func Test_ReadData(t *testing.T) {
 	RegisterTestingT(t)
 
+	grid, width, height := getData(Entry.PuzzleInput())
+
+	Expect(grid[0][0]).Should(Equal(2))
+	Expect(grid[0][width-1]).Should(Equal(3))
+	Expect(grid[height-1][0]).Should(Equal(2))
+	Expect(grid[height-1][width-1]).Should(Equal(3))
+
+}
+
+func Test_TestScenicScore(t *testing.T) {
+	RegisterTestingT(t)
+	grid, _, _ := getData(testData)
+
+	Expect(getScenicScore(grid, common.Point{2, 1})).Should(Equal(4))
+	Expect(getScenicScore(grid, common.Point{2, 3})).Should(Equal(8))
+}
+
+func Test_TestFromUp(t *testing.T) {
+	RegisterTestingT(t)
+	grid, _, _ := getData(testData)
+	canSee := canViewFromUp(grid, common.Point{2, 1})
+
+	Expect(canSee).Should(Equal(true))
+}
+
+func Test_CountTrees(t *testing.T) {
+	RegisterTestingT(t)
+
+	grid, _, _ := getData(testData)
+
+	Expect(getTreeCount(grid)).Should(Equal(21))
 }
 
 func Test_PartOne(t *testing.T) {
 	RegisterTestingT(t)
 
+	Expect(Entry.PartOne(Entry.PuzzleInput(), nil)).Should(Equal("1681"))
 }
 
 func Test_PartTwo(t *testing.T) {
 	RegisterTestingT(t)
 
+	Expect(Entry.PartTwo(Entry.PuzzleInput(), nil)).Should(Equal("201684"))
 }
 
 func Benchmark_BenchPartOne(b *testing.B) {
